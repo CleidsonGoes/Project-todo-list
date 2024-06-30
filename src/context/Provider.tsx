090e57d080466175974e096b9ab89919a435120b
CleidsonGoes <jorgegoes4@gmail.com>
2024-06-30 20:24:25 -0300

    introduzindo o Context

---
+++ src/context/Provider.tsx
@@ -0,0 +1,12 @@
+
+type ProviderProps = {
+    children: React.ReactNode
+}
+
+export type ProviderValues = {
+    // aqui serão as Info. que serão armazenadas
+}
+
+function Provider({children}: ProviderProps) {
+
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,37 @@
+import { ChangeEvent, FormEvent, useState } from "react"
 import imageLogin from "../assets/pngwing.com (1).png"
+import { useNavigate } from "react-router-dom";
+
 
 function Login() {
+
+    const navigate = useNavigate();
+
+    // const [username, setUsername] = useState('')
+    // const [password, setPassword] = useState('')
+    
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
+        setInputs({...inputs, [event.target.name]: [event.target.value]});
+    }
+
+    const handleSubmit = (event: FormEvent) => {
+        // para evitar o carregamento padrão do botão de enviar
+        event.preventDefault();
+        navigate("/todo")
+    }
+
     return(
         <>
         <h1>Welcome</h1>
         <img src={imageLogin} alt="" />
-        <form action="">
-            <input type="text" name="username" id="username" placeholder="Enter your username"/>
-            <input type="password" name="password" id="password" placeholder="Enter your password"/>
+        <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} type="text" name="username" id="username" placeholder="Enter your username"/>
+            <input onChange={handleChange} type="password" name="password" id="password" placeholder="Enter your password"/>
             <p>Forget Password?</p>
             <button>Sign In</button>
         </form>
